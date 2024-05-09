54988156d111f32529acc8f15a7728a057b5dcdf
ratatyiii <woolfichannel@gmail.com>
2024-05-09 04:51:24 +0300

    Added username column

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,10 +25,11 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'username' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'remember_token' => Str::random(36),
         ];
     }
 
