7a7c20e7658d77d4d60cf627d910b2cc22b3e744
ratatyiii <woolfichannel@gmail.com>
2024-05-13 02:57:27 +0300

    Added docker

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+        watch: {
+            usePolling: true,
+        },
+        host: true,
+
+    },
     plugins: [
         laravel({
             input: ['resources/js/app.js'],
